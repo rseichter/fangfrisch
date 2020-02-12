98786992fd2f817a9b71ae77b6426cbda575ea27
Ralph Seichter <github@seichter.de>
2020-02-12 18:10:37 +0100

    Define action choices

--- fangfrisch/__main__.py
+++ fangfrisch/__main__.py
@@ -23,17 +23,20 @@ from fangfrisch.config.config import config
 from fangfrisch.logging import log
 from fangfrisch.refresh import ClamavRefresh
 
+DUMP = 'dumpconf'
+REFRESH = 'refresh'
+
 parser = argparse.ArgumentParser()
-parser.add_argument('action', help='Action to perform')
+parser.add_argument('action', choices=[DUMP, REFRESH], help='Action to perform')
 parser.add_argument('-c', '--conf', default=None, help='Configuration file')
 parser.add_argument('-f', '--force', default=False, action='store_true', help='Force action (default: False)')
 args = parser.parse_args()
 if not config.init(args.conf):
     log.error(f'Cannot parse configuration file: {args.conf}')
     sys.exit(1)
-if 'dumpconf' == args.action:
+if DUMP == args.action:
     config.dump()
-elif 'refresh' == args.action:
+elif REFRESH == args.action:
     ClamavRefresh(args).refresh_all()
 else:
     log.error(f'Unknown action: {args.action}')
