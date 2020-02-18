198333a5f858c14c64e2dafb568b3701a1fb4368
Ralph Seichter <github@seichter.de>
2020-02-18 03:36:17 +0100

    Add timeout for GET requests

--- fangfrisch/download.py
+++ fangfrisch/download.py
@@ -64,7 +64,7 @@ def _download(url, max_length: int) -> StatusDataPair:
     :param max_length: Maximum permitted content length.
     :return: True/Data for successfull downloads, False/None otherwise.
     """
-    response = _session.get(url, stream=True)
+    response = _session.get(url, stream=True, timeout=30)
     if response.status_code != requests.codes.ok:
         log.error(f'{url} download failed: {response.status_code} {response.reason}')
         return StatusDataPair(False)
