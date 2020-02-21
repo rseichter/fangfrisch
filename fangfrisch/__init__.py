a5f5c65516173e345ca3cac47310020f3d9fe853
Ralph Seichter <github@seichter.de>
2020-02-21 11:41:00 +0100

    Version bump

--- fangfrisch/__init__.py
+++ fangfrisch/__init__.py
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Fangfrisch. If not, see <https://www.gnu.org/licenses/>.
 """
-__version__ = '0.1rc2'
+__version__ = '1.0.0'
 
 
 class ClamavItem:
